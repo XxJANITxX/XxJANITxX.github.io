15c5b3051cf6c8734d7e7e715240c9f4edba528b
Janit <janitgarg12345@gmail.com>
2023-10-31 11:12:32 +0530

    Added 'About' Component for small time to see dark mode feature. We created style object and passed as a parameter in it.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 
+
 function App() {
-  return (
-    <>
-      {/* Passing prop variables (video 6) */}
-      <Navbar title="Text Utils" about="About US"/>
-      <div className="container my-3">
-        <Textform heading="Enter the text to analyze"/>
-      </div>
-    </>
-  );
+    return (
+        <>
+            {/* Passing prop variables (video 6) */}
+            <Navbar title="Text Utils" about="About US" />
+            <div className="container my-3">
+                <Textform heading="Enter the text to analyze" />
+                {/* <About /> */}
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,72 @@
+// This component is created for video 10
+
+import React, {useState} from "react";
+
+export default function About() {
+
+    const [myStyle, setMyStyle] = useState({
+        color:'black',
+        backgroundColor:'white',
+        border:'1px solid black'
+    });
+
+    const [buttonName, setButtonName] = useState("Enable dark mode");
+
+    const darkModeEnable = (event) =>{
+
+        if(myStyle.color === "white"){
+            setMyStyle({
+                color:'black',
+                backgroundColor:'white',
+                border:'1px solid black'
+            });
+            setButtonName("Enable light mode");
+        }else{
+            setMyStyle({
+                color:'white',
+                backgroundColor:'black',
+                border:'1px solid white'
+            });
+            setButtonName("Enable dark mode");
+        }
+    }
+
+    return (
+        <div className="container" style={myStyle}>
+            <h1 className="my-2">About Us</h1>
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            style={myStyle}
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseOne"
+                            aria-expanded="false"
+                            aria-controls="flush-collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="flush-collapseOne"
+                        className="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushExample"
+                    >
+                        <div className="accordion-body">
+                            Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the
+                            first item's accordion body.
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className="container">
+                <button type="button" className="btn btn-primary" onClick={darkModeEnable}>{buttonName}</button>
+            </div>
+        </div>
+    );
+}
